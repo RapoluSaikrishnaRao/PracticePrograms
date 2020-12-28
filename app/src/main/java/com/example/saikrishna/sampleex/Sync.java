c1c1c462b3596449a3e26eab9f292f14c84c4b12
rapol <rapolesaikrishna@gmail.com>
2020-12-28 14:31:54 +0530

    Sync Class code updated.

--- app/src/main/java/com/example/saikrishna/sampleex/Sync.java
+++ app/src/main/java/com/example/saikrishna/sampleex/Sync.java
@@ -1,50 +1,47 @@
 package com.example.saikrishna.sampleex;
 
 public class Sync {
-    public static void main(String[] args) {
-        PrintClass printClasss = new PrintClass();
-        MyThreads myThreads =new MyThreads(printClasss);
-        MyThreads myThreads1 =new MyThreads(printClasss);
+    public static void main(String args[])
+    {
+        FirstThread first =new FirstThread();
+        FirstThread1 firstThread = new FirstThread1(first);
+        FirstThread1 firstThread2 = new FirstThread1(first);
+        firstThread.start();
+        firstThread2.start();
 
-        Thread thread =new Thread(myThreads);
-        Thread thread1 =new Thread(myThreads1);
-
-        thread.start();
-        thread1.start();
     }
 }
+class FirstThread
+{
 
-class PrintClass {
-     public void printMethod(int number) {
-         synchronized (this){
-        for (int i = 0; i <= number; i++) {
-            System.out.println(i);
-            try {
-                Thread.sleep(300);
-            } catch (Exception e) {
 
+
+    public  void Print(int n)
+    {
+
+        synchronized(this){
+            for (int i = 0; i <= n; i++) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        }
     }
 }
-
-class MyThreads extends Thread {
-    PrintClass printClass;
-
-    MyThreads(PrintClass printClass) {
-        this.printClass = printClass;
+class FirstThread1 extends Thread implements Runnable
+{
+    FirstThread firstThread;
+    public FirstThread1(FirstThread first) {
+        this.firstThread=first;
     }
 
     @Override
-    public void run() {
-        printClass.printMethod(10);
+    public void run()
+    {
+        firstThread.Print(5);
     }
 }
-
-
-
-
-
-
 
